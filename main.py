68fed3f4048cb7dd50f91016badd119f5dc2e4bf
Burak Arabacioglu <burak.arabacioglu.work@gmail.com>
2026-05-15 17:23:12 +0300

    Complete CRUD logic.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, Path, HTTPException
+from pydantic import BaseModel, Field
 from typing import Annotated
 from sqlalchemy.orm import Session
 from starlette import status
@@ -10,6 +11,11 @@ app = FastAPI()
 
 Base.metadata.create_all(engine)
 
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3),
+    description: str = Field(min_length=3, max_length=1000)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
 
 
 def get_db():
@@ -31,3 +37,30 @@ async def get_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     if todo is not None:
         return todo
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+@app.post("/create_todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(todo_request: TodoRequest, db: Session = Depends(get_db)):
+    todo = Todo(**todo_request.model_dump())
+    db.add(todo)
+    db.commit()
+
+@app.put("/update_todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id : int = Path(gt=0)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if todo is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+    todo.title = todo_request.title
+    todo.description = todo_request.description
+    todo.priority = todo_request.priority
+    todo.complete = todo_request.complete
+
+    db.add(todo)
+    db.commit()
+
+@app.delete("/delete_todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if todo is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    db.delete(todo)
+    db.commit()
