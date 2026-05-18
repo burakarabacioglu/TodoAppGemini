fb8b4fc216693e324e4e8652ca646cf7193930da
Burak Arabacioglu <burak.arabacioglu.work@gmail.com>
2026-05-18 12:54:35 +0300

    Refine auth logic and create user model.

--- routers/auth.py
+++ routers/auth.py
@@ -1,10 +1,29 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+
+from models import User
 
 router = APIRouter(
     prefix="/auth",
     tags=["Authentication"],
 )
 
-@router.get("/")
-async def get_helloworld():
-    return "Hello World"
+class CreateUserRequest(BaseModel):
+    email: str
+    username: str
+    first_name: str
+    last_name: str
+    hashed_password: str
+    role: str
+
+@router.post("/auth")
+async def create_user(create_user_request: CreateUserRequest):
+    user = User(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        is_active=True,
+        hashed_password=create_user_request.hashed_password,
+        role=create_user_request.role
+    )
