88d1c256da4b58ca1b30d9ef8de6a2f3a7d19d56
Burak Arabacioglu <burak.arabacioglu.work@gmail.com>
2026-05-28 17:15:42 +0300

    feat: create descriptions for todos with genai.

--- routers/todo.py
+++ routers/todo.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 from fastapi import APIRouter, Depends, Path, HTTPException, Request
 from pydantic import BaseModel, Field
 from typing import Annotated
@@ -5,6 +6,7 @@ from sqlalchemy.orm import Session
 from starlette import status
 from starlette.responses import RedirectResponse
 import logging
+import markdown
 
 from database import SessionLocal
 from models import Todo
@@ -12,6 +14,11 @@ from routers.auth import get_current_user
 from fastapi.templating import Jinja2Templates
 from routers.auth import SECRET_KEY, ALGORITHM
 from jose import JWTError, jwt
+from dotenv import load_dotenv
+import google.generativeai as genai
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage, AIMessage
 
 logger = logging.getLogger(__name__)
 router = APIRouter(
@@ -135,6 +142,7 @@ async def create_todo(user: user_dependency, todo_request: TodoRequest, db: Sess
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     todo = Todo(**todo_request.model_dump(), owner_id=user.get('id'))
+    todo.description = create_todo_with_gemini(todo.description)
     db.add(todo)
     db.commit()
 
@@ -162,3 +170,23 @@ async def delete_todo(user: user_dependency, db: db_dependency, todo_id: int = P
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     db.delete(todo)
     db.commit()
+
+def markdown_to_text(markdown_string):
+    html = markdown.markdown(markdown_string)
+    soup = BeautifulSoup(html, "html.parser")
+    text = soup.get_text()
+    return text
+
+
+def create_todo_with_gemini(todo_string: str):
+    load_dotenv()
+    genai.configure(api_key=os.environ.get("GOOGLE_API_KEY"))
+    llm=ChatGoogleGenerativeAI(model="gemini-flash-latest")
+    response = llm.invoke(
+        [
+            HumanMessage(
+                content="I will provide you a todo item to add my to do list. What i want you to do is to create a longer and more comprehensive description of that todo item, my next message will be my todo:"),
+            HumanMessage(content=todo_string),
+        ]
+    )
+    return markdown_to_text(response.content)
